188a87d7e9bb341dcdec67b0636a9464dec25c30
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-10-26 10:03:22 -0400

    tutu 26/10

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
-app.post('/filmes' , (req,res)=>{
-    res.json("filme cadastro com sucesso")
-})
-app.get('/filmes', (req,res)=>{
-    res.send("Listados com sucesso")
-})
+import express from 'express';
+const app = express();
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Servidoe rodando na porta 3000')
-})
-export default app
+// Tenho que ter uma rota POST para cadastrar um filme
+
+// Salvar em algum lugar o filme que foi cadastrado.
+type Filme = {
+    id: number,
+    titulo: string,
+    descricao: string,
+    imagem: string
+}
+let filmesCadastros: Filme[] = []
+
+app.post('/filmes', (req, res) => {
+    const { id, titulo, descricao, imagem } = req.body;
+    const filme: Filme = {
+        id,
+        titulo,
+        descricao,
+        imagem
+    }
+
+    // Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
+    filmesCadastros.push(filme);
+    res.status(201).send(filme);
+});
+
+app.get('/filmes', (req, res) => {
+    res.status(200).send(filmesCadastros); // Retorna a lista de filmes cadastrados
+});
+
+app.get('/filmes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    // Usar o método find para buscar um filme pelo id e retornar para o usuário
+    const filmeEncontrado = filmesCadastros.find(filme => filme.id === id);
+
+    if (filmeEncontrado) {
+        res.status(200).send(filmeEncontrado);
+    } else {
+        res.status(404).send("Filme não encontrado");
+    }
+});
+
+// Tenho que iniciar o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,77 @@
-import {test,expect,describe} from 'vitest'
-import axios from 'axios'
-describe('Primeiro teste da aplicação',()=>{
-    test('Espero que 1 seja igual a 1',()=>{
-    expect(1).toBe(1)
-    })
-})
-describe('Cadastro de filmes',()=>{
-    test('Espero que o filme seja cadastrado',async ()=>{
+import { test, expect, describe } from 'vitest';
+
+describe('Primeiro teste da aplicação', () => {
+    test('Espero que 1 seja igual a 1', () => {
+        expect(1).toBe(1);
+    });
+});
+
+describe('Cadastro de filmes', () => {
+    test('Espero que o filme seja cadastrado', async () => {
         const filme = {
-            titulo:'Vingadores',
-            descricao:'Filme dos herois da marvel',
-            imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
-        }
-        //Passar isso para minha API
-        const resposta = await axios.post('http://localhost:3000/filmes',filme)
-        //Esperar que minha API tenha cadastrado o filme
-        expect(resposta.status).toBe(201)
-        expect(resposta.data).toEqual(filme)
-    })
-})
+            id: 1,
+            titulo: 'Vingadores',
+            descricao: 'Filme dos heróis da Marvel',
+            imagem: "https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        };
+
+        // Substitua a URL com a rota correta para cadastrar filmes na sua API.
+        const resposta = await fetch('http://localhost:3000/filmes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(filme)
+        });
+
+        expect(resposta.status).toBe(201);
+        expect(await resposta.json()).toEqual(filme);
+    });
+
+    test('Espero que cadastre um segundo filme', async () => {
+        const filme = {
+            id: 2,
+            titulo: 'Homem de Ferro',
+            descricao: 'Filme do Homem de Ferro',
+            imagem: "https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        };
+
+        // Substitua a URL com a rota correta para cadastrar filmes na sua API.
+        const resposta = await fetch('http://localhost:3000/filmes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(filme)
+        });
+
+        expect(resposta.status).toBe(201);
+        expect(await resposta.json()).toEqual(filme);
+    });
+
+    test('Espero que o filme cadastrado seja listado', async () => {
+        const filme = {
+            id: 2,
+            titulo: 'Homem de Ferro',
+            descricao: 'Filme do Homem de Ferro',
+            imagem: "https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        };
+
+        // Substitua a URL com a rota correta para cadastrar e listar filmes na sua API.
+        const resposta = await fetch('http://localhost:3000/filmes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(filme)
+        });
+
+        expect(resposta.status).toBe(201);
+        expect(await resposta.json()).toEqual(filme);
+
+        // Substitua a URL com a rota correta para listar todos os filmes na sua API.
+        const respostaGet = await fetch('http://localhost:3000/filmes');
+        expect(respostaGet.status).toBe(200);
+        expect(await respostaGet.json()).toContainEqual(filme);
+    });
+});
