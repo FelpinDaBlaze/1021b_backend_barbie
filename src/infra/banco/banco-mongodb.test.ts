f5e0b4f0530513c75e2ad50861a981583696574e
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-11-09 10:13:06 -0400

    aulinha 09/11

--- src/infra/banco/banco-mongodb.test.ts
+++ src/infra/banco/banco-mongodb.test.ts
@@ -2,17 +2,18 @@ import {describe,test, expect} from 'vitest'
 import {BancoMongoDB} from './banco-mongodb'
 
 describe('BancoMongoDB teste', () => {
+    const bancoMongoDB = new BancoMongoDB()
     test('BancoMongoDB', () => {
-        const bancoMongoDB = new BancoMongoDB()
         expect(bancoMongoDB).toBeDefined()
     })
-    test('deve salvar um filme', () => {
-        const bancoMongoDB = new BancoMongoDB()
+    test('deve salvar um filme', async () => {
         const filme = {
-            id: '1',
+            id: 1,
             titulo: 'O Poderoso Chefão',
             descricao: 'Filme de máfia',
-            foto: 'fotofilme.jpg'
+            imagem: 'fotofilme.jpg'
         }
+        const result = await bancoMongoDB.salvar(filme)
+        expect(result).toEqual(filme)
     })
 })
