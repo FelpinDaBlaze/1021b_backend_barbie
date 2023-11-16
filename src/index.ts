8698e950deb4ea693165587b696526c452ce1a89
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-11-16 10:12:49 -0400

    post

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,51 @@
 import express from 'express'
+import ListaFilme from './aplicacao/lista_filme.use-case'
+import BancoMongoDB from './infra/banco/banco_mongodb'
+import BancoEmMemoria from './infra/banco/banco_em_memoria'
+import SalvaFilme from './aplicacao/salva_filme.use-case'
 const app = express()
-app.use(express.json())
-import ListaFilme from './aplicacao/lista-filme.use-case'
-import BancoMongoDB from './infra/banco/banco-mongodb'
 const bancoMongoDB = new BancoMongoDB()
-app.get('/filmes',async (req,res)=>{
-    //usem o listarFilme Usecase para listar os filmes
-    const listaFilme = new ListaFilme(bancoMongoDB)
-    const filmes = await listaFilme.executar()
-    res.send(filmes)
-})
-app.post('/filmes',(req,res)=>{
-    const {id,titulo,descricao,imagem} = req.body
-    const filme = {
-        id,
-        titulo,
-        descricao,
-        imagem
-    }
-    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
-    filmesCadastros.push(filme)
-    res.status(201).send(filme)
-})
-
-app.get('/filmes/:id',(req,res)=>{
-    const id = parseInt(req.params.id)
-    //FIND para buscar um filme pelo id e retornar para o usuário
-    const filme = filmesCadastros.find(filme => filme.id === id)
-    if(!filme) return res.status(404).send("Filme não encontrado")
-    res.status(200).send(filme)
-})
-
-//Tenho que iniciar o servidor na porta 3000
-app.listen(3000,()=>{
-    console.log('Servidor rodando na porta 3000')
-})
-
+app.use(express.json())
 //Tenho que ter uma rota post para cadastrar um filme
-
-//Salvar em algum lugar o filme que foi cadastrado.
 type Filme = {
     id:number,
     titulo:string,
     descricao:string,
     imagem:string
 }
-let filmesCadastros:Filme[] = []
+let filmesCadastrados:Filme[] = []
+
+app.post('/filmes', async(req,res)=>{
+    const {id, titulo, descricao, imagem} = req.body
+    const filme = {
+        id,
+        titulo,
+        descricao,
+        imagem
+    }
+
+    //como salvo o filme
+    const salvarFilme = new SalvaFilme(bancoMongoDB)
+    const filmes = await salvarFilme.execute(filme)
+    // filmesCadastrados.push(filme)
+    res.status(201).send(filmes)
+})
+
+app.get('/filmes', async(req,res)=>{
+    //usem o listarFilme para listar os filmes
+    const listaFilme = new ListaFilme(bancoMongoDB)
+    const filmes = await listaFilme.executar()
+    res.send(filmes)
+})
+
+app.get('/filmes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const filmeId = filmesCadastrados.find(Filme => Filme.id == id);
+    if(!filmeId) return res.status(404).send("Filme não encontrado");
+    res.status(200).send(filmeId);      
+});
+
+//Tenho que iniciar o servidor na porta 3000
+app.listen(3000,()=>{
+    console.log('Servidor rodando na porta 3000')
+})
